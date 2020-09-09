927d46ea42213d4bc974579bfc17da49b6a7da35
Filip Janevski <filip.janevski@joingsg.com>
2020-09-09 17:41:14 +0200

    no message

--- packages/cache-url/index.d.ts
+++
@@ -1,9 +0,0 @@
-export class AmpToolboxCacheUrl {
-  static createCacheUrl(
-    domainSuffix: string,
-    url: string
-  ): Promise<string>;
-  static createCurlsSubdomain(
-    url: string,
-  ): Promise<string>;
-}
